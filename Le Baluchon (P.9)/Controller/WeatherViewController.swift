7ee40e645df66e09492c497a839bf22e7989844b
Fardi <fardi01.pro@gmail.com>
2021-09-21 23:14:29 +0200

    init project

---
+++ Le Baluchon (P.9)/Controller/WeatherViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MeteoViewController.swift
+//  Le Baluchon (P.9)
+//
+//  Created by fardi Issihaka on 19/09/2021.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
